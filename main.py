e5dd5a48b965fcbda46a59365814899ca8a629cc
riadabduragimov <102159026+riadabduragimov@users.noreply.github.com>
2025-04-22 17:20:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,12 @@
-import random
-import matplotlib.pyplot as plt
 from gridworld_env import GridWorldEnv
-from double_q_learning import DoubleQLearning
-from utils import extract_policy, evaluate_agent
+from q_learning import QLearning, extract_policy, evaluate_agent
+import random
 
 if __name__ == "__main__":
-    m = 5
-    all_obstacles = [(1, 1), (2, 2), (3, 3), (1, 3), (4, 4)]
-    for i in range(m):
-        print(f"\n--- GridWorld Environment {i+1} ---")
-        grid_size = random.choice([5, 7, 10])
-        goal_state = (random.randint(0, grid_size - 1), random.randint(0, grid_size - 1))
-        obstacles = random.sample([obs for obs in all_obstacles if obs != goal_state], 3)
-        env = GridWorldEnv(grid_size=grid_size, goal_state=goal_state, stochastic=True, obstacles=obstacles)
-        
-        print("Initial Grid:")
-        env.render()
-
-        agent = DoubleQLearning(env)
-        rewards = agent.train(episodes=5000)
-
-        plt.plot(rewards)
-        plt.title("Episode Rewards Over Time")
-        plt.xlabel("Episodes")
-        plt.ylabel("Total Reward")
-        plt.grid(True)
-        plt.show()
-
-        policy = extract_policy(agent.Q1, env)
-        print("Final Policy Grid:")
-        env.render(policy=policy)
-        evaluate_agent(env, policy)
+    GRID_SIZE = 40
+    goal = (random.randint(0, GRID_SIZE - 1), random.randint(0, GRID_SIZE - 1))
+    env = GridWorldEnv(grid_size=GRID_SIZE, goal_state=goal, stochastic=True)
+    agent = QLearning(env, world_id=0)
+    rewards = agent.train(episodes=1500)
+    policy = extract_policy(agent.Q, env)
+    evaluate_agent(env, policy, episodes=10)
